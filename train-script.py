2077547e18dfe97f0f6a4d1af5f1f347ecc95ca8
miniBear <ax_zaa@windowslive.com>
2019-10-18 20:17:37 +0700

    print args gen_command

--- train-script.py
+++ train-script.py
@@ -7,9 +7,9 @@ train_file = train
 # train_file = train_neg
 
 with open(train_file) as f:
-  print(len(f.readlines()))
-  train_size = len(f.readlines())
-  f.close
+    print(len(f.readlines()))
+    train_size = len(f.readlines())
+    f.close
 
 
 test_file = 'test_0.85-333.csv'
@@ -17,156 +17,186 @@ class_file = 'classes'
 
 weights = "resnet50_coco_best_v2.1.0.h5"
 
-max_side=700
-min_side=700
+max_side = 700
+min_side = 700
 
-epochs= 10
+epochs = 10
 #epochs= 1
 
-batch_size=1
+batch_size = 1
 
-steps=int(train_size//1)
-steps= 3000
+steps = int(train_size//1)
+steps = 2500
 print(steps)
 
 cmds = {}
-base_path = os.getcwd() +'/store_models'
+base_path = os.getcwd() + '/store_models'
+
+default_args = {
+    'train_file': train_file,
+    'test_file': test_file,
+    'class_file': class_file,
+    'weights': "resnet50_coco_best_v2.1.0.h5",
+    'max_side': 700,
+    'min_side': 700,
+    'epochs': epochs,
+    'batch_size': batch_size,
+    'steps': steps,
+}
+
 
 def create_dir(backbone, path):
-  snapshot_path = base_path +'/'+backbone+'/'+ path + '/snapshots'
-  tensorboard_dir = base_path +'/'+backbone+'/'+ path + '/logs'
+    snapshot_path = base_path + '/'+backbone+'/' + path + '/snapshots'
+    tensorboard_dir = base_path + '/'+backbone+'/' + path + '/logs'
 
-  # Create target directory & all intermediate directories if don't exists
-  try:
-      os.makedirs(snapshot_path)    
-      print("Directory " , snapshot_path ,  " Created ")
-  except FileExistsError:
-      print("Directory " , snapshot_path ,  " already exists")  
+    # Create target directory & all intermediate directories if don't exists
+    try:
+        os.makedirs(snapshot_path)
+        print("Directory ", snapshot_path,  " Created ")
+    except FileExistsError:
+        print("Directory ", snapshot_path,  " already exists")
 
-  try:
-      os.makedirs(tensorboard_dir)    
-      print("Directory " , tensorboard_dir ,  " Created ")
-  except FileExistsError:
-      print("Directory " , tensorboard_dir ,  " already exists") 
+    try:
+        os.makedirs(tensorboard_dir)
+        print("Directory ", tensorboard_dir,  " Created ")
+    except FileExistsError:
+        print("Directory ", tensorboard_dir,  " already exists")
 
-  return snapshot_path, tensorboard_dir
-      
+    return snapshot_path, tensorboard_dir
+
+
 def optimize_anchor():
-    cmd = "python3 optimize_anchors.py '/home/minibear/Desktop/pre-data/pigeon.csv' --no-resize --ratios=5 --image-max-side 700 --image-min-side 700"
-  
+    cmd, args = "python3 optimize_anchors.py '/home/minibear/Desktop/pre-data/pigeon.csv' --no-resize --ratios=5 --image-max-side 700 --image-min-side 700"
+
+
 def debug():
-      cmd = "retinanet-debug --anchors --display-name --annotations --config retina-5r-3s.ini csv train_neg_0.85-1885-332-2217.csv classes"
+    cmd, args = "retinanet-debug --anchors --display-name --annotations --config retina-5r-3s.ini csv train_neg_0.85-1885-332-2217.csv classes"
 
-def gen_command(train_file=None, test_file=None, class_file=None,  weights= None,
-    backbone = "resnet50", max_side= 700, min_side = 700, batch_size= 1, epochs=10, steps= 1000,
-    anchor_config = None, snapshot_path=None, tensorboard_dir=None, store_path=None):
-  snapshot_path, tensorboard_dir = create_dir(backbone, store_path)
 
-  cmd = "retinanet-train --backbone {backbone}\
-    --weights {weights} --image-max-side {max} --image-min-side {min}\
+def gen_command(train_file=None, test_file=None, class_file=None,  weights=None,
+                backbone="resnet50", max_side=700, min_side=700, batch_size=1, epochs=10, steps=1000,
+                anchor_config=None, snapshot_path=None, tensorboard_dir=None, store_path=None, snapshot=None):
+
+    snapshot_path, tensorboard_dir = create_dir(backbone, store_path)
+
+    cmd = "retinanet-train {snapshot} --backbone {backbone}\
+     {weights} --image-max-side {max} --image-min-side {min}\
     --batch-size {batch_size} --epochs {epochs} --steps {steps} --weighted-average --compute-val-loss\
       {snapshot_path} {tensorboard_dir} {anchor_config}\
       csv {train_file} {class_file} --val-annotations {test_file} "\
-    .format(backbone='"'+backbone+'"', max = max_side, min=min_side,
-        batch_size=batch_size, epochs=epochs, steps=steps,
-        weights=weights or "resnet50_coco_best_v2.1.0.h5",
-        train_file=train_file or 'train_neg_0.85-1885-332-2217.csv',
-        class_file= class_file or 'classes',
-        test_file=test_file or 'test_0.85-333.csv',
-        anchor_config = '--config ' + anchor_config if anchor_config else '',
-        snapshot_path = '--snapshot-path ' + snapshot_path if snapshot_path else '',
-        tensorboard_dir = '--tensorboard-dir ' + tensorboard_dir if tensorboard_dir else '')
-  return cmd
+      .format(backbone='"'+backbone+'"', max=max_side, min=min_side,
+              batch_size=batch_size, epochs=epochs, steps=steps,
+              weights='--weights ' +
+              weights or "--weights resnet50_coco_best_v2.1.0.h5" if not snapshot else '',
+              train_file=train_file or 'train_neg_0.85-1885-332-2217.csv',
+              class_file=class_file or 'classes',
+              test_file=test_file or 'test_0.85-333.csv',
+              anchor_config='--config ' + anchor_config if anchor_config else '',
+              snapshot_path='--snapshot-path ' + snapshot_path if snapshot_path else '',
+              tensorboard_dir='--tensorboard-dir ' +
+              tensorboard_dir if tensorboard_dir else '',
+              snapshot='--snapshot '+snapshot if snapshot else '')
+
+    args = {
+        'train_file': train_file,
+        'test_file': test_file,
+        'class_file': class_file,
+        'weights': weights,
+        'backbone': backbone,
+        'max_side': max_side,
+        'min_side': min_side,
+        'batch_size': batch_size,
+        'epochs': epochs,
+        'steps': steps,
+        'anchor_config': anchor_config,
+        'snapshot_path': snapshot_path,
+        'tensorboard_dir': tensorboard_dir,
+        'store_path': store_path,
+        'snapshot': snapshot
+    }
+    return cmd, args
+
 
 def test_cmd():
-  return gen_command(epochs=1, steps=10)
+    return gen_command(epochs=1, steps=10)
 
 
-### resnet50 700 700
+# resnet50 700 700
 backbone = 'resnet50'
 #	default anchor
-cmd = gen_command(train_file= train_file,
-                  test_file= test_file,
-                  class_file= class_file,
-                  weights=weights,
-                  store_path='default',
-                  backbone = backbone, max_side= max_side, min_side = min_side, batch_size= batch_size, epochs=epochs, steps= steps)
+cmd, args = gen_command(store_path='default',
+                        backbone=backbone, **default_args)
 
-cmds['0'] = {'name':'default', 'cmd':cmd}
+cmds['0'] = {'name': 'default', 'cmd': cmd, 'backbone': backbone, 'args': args}
 
-#	anchor	3r 
+#	anchor	3r
 anchor_3r = 'retinanet-3.ini'
-cmd = gen_command(train_file= train_file,
-                  test_file= test_file,
-                  class_file= class_file,
-                  weights=weights,
-                  anchor_config=anchor_3r,
-                  store_path='anchor_3r',
-                  backbone = backbone, max_side= max_side, min_side = min_side, batch_size= batch_size, epochs=epochs, steps= steps)
+cmd, args = gen_command(anchor_config=anchor_3r,
+                        store_path='anchor_3r',
+                        backbone=backbone, **default_args)
 
-cmds['1'] = {'name':'anchor_3r', 'cmd':cmd}
+cmds['1'] = {'name': 'anchor_3r', 'cmd': cmd,
+             'backbone': backbone, 'args': args}
 
 
-#	anchor	5r 3s 
+#	anchor	5r 3s
 anchor_5r_3s = 'retina-5r-3s.ini'
-cmd = gen_command(train_file= train_file,
-                  test_file= test_file,
-                  class_file= class_file,
-                  weights=weights,
-                  anchor_config=anchor_5r_3s,
-                  store_path='anchor_5r_3s',
-                  backbone = backbone, max_side= max_side, min_side = min_side, batch_size= batch_size, epochs=epochs, steps= steps)
+cmd, args = gen_command(anchor_config=anchor_5r_3s,
+                        store_path='anchor_5r_3s',
+                        backbone=backbone, **default_args)
 
-cmds['2'] = {'name':'anchor_5r_3s', 'cmd':cmd}
+cmds['2'] = {'name': 'anchor_5r_3s', 'cmd': cmd,
+             'backbone': backbone, 'args': args}
 
+default_args = {
+    'train_file': train_file,
+    'test_file': test_file,
+    'class_file': class_file,
+    'weights': "resnet50_coco_best_v2.1.0.h5",
+    'max_side': 400,
+    'min_side': 400,
+    'epochs': epochs,
+    'batch_size': batch_size,
+    'steps': steps,
+}
 
-max_side=400
-min_side=400
-
-### resnet101 300 300
+# resnet101 300 300
 backbone = 'resnet101'
 #	default anchor
-cmd = gen_command(train_file= train_file,
-                  test_file= test_file,
-                  class_file= class_file,
-                  weights=weights,
-                  store_path='default',
-                  backbone = backbone, max_side= max_side, min_side = min_side, batch_size= batch_size, epochs=epochs, steps= steps)
+cmd, args = gen_command(store_path='default',
+                        snapshot='store_models/resnet101/default/snapshots/resnet101_01_loss-26.9501_val-loss-2.5892_mAP-0.3225.h5',
+                        backbone=backbone, **default_args)
 
-cmds['3'] = {'name':'default', 'cmd':cmd}
+cmds['3'] = {'name': 'default', 'cmd': cmd, 'backbone': backbone, 'args': args}
 
-#	anchor	3r 
+#	anchor	3r
 anchor_3r = 'retinanet-3.ini'
-cmd = gen_command(train_file= train_file,
-                  test_file= test_file,
-                  class_file= class_file,
-                  weights=weights,
-                  anchor_config=anchor_3r,
-                  store_path='anchor_3r',
-                  backbone = backbone, max_side= max_side, min_side = min_side, batch_size= batch_size, epochs=epochs, steps= steps)
+cmd, args = gen_command(anchor_config=anchor_3r,
+                        store_path='anchor_3r',
+                        backbone=backbone, **default_args)
 
-cmds['4'] = {'name':'anchor_3r', 'cmd':cmd}
+cmds['4'] = {'name': 'anchor_3r', 'cmd': cmd,
+             'backbone': backbone, 'args': args}
 
 
-#	anchor	5r 3s 
+#	anchor	5r 3s snapshot
 anchor_5r_3s = 'retina-5r-3s.ini'
-cmd = gen_command(train_file= train_file,
-                  test_file= test_file,
-                  class_file= class_file,
-                  weights=weights,
-                  anchor_config=anchor_5r_3s,
-                  store_path='anchor_5r_3s',
-                  backbone = backbone, max_side= max_side, min_side = min_side, batch_size= batch_size, epochs=epochs, steps= steps)
-
-cmds['5'] = {'name':'anchor_5r_3s', 'cmd':cmd}
+cmd, args = gen_command(anchor_config=anchor_5r_3s,
+                        store_path='anchor_5r_3s',
+                        backbone=anchor_5r_3s, **default_args)
+cmds['5'] = {'name': 'anchor_5r_3s', 'cmd': cmd,
+             'backbone': backbone, 'args': args}
 
 if __name__ == '__main__':
-  run = True
-  #run = False
-  # for i in cmds:
-  #   print("{i}\n{cmd}".format(i=i, cmd=cmds[i]))
-    
-  if run:
-      tmp_cmd = cmds['3']
-      print('{}\n\n{}\n\n{}'.format('#'*30, tmp_cmd['name'], '#'*30))
-      os.system(tmp_cmd['cmd']) 
+    run = True
+    #run = False
+    # for i in cmds:
+    #   print("{i}\n{cmd}".format(i=i, cmd=cmds[i]))
+
+    if run:
+        tmp_cmd = cmds['0']
+        print('{s}\n\nname: {name}\nbackbone: {backbone}\n\n{args}\n\n{n}'.format(s='#'*30, n='#'*30,
+                                                                                name=tmp_cmd['name'],
+                                                                                backbone=tmp_cmd['backbone'],
+                                                                                args='\n'.join(sorted(['{: <20}{}'.format(k, str(v)) for k, v in tmp_cmd['args'].items()]))))
+        os.system(tmp_cmd['cmd'])
